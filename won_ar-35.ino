7cf1097194759dfc14c4ac3c00f11d9dca8f6a3e
won <jonwon2009@naver.com>
2022-05-12 00:07:44 +0900

    정수 pow, 정수 root 완료
    정수 pow는 뉴튼-랩튼 이용함. 앞으로 다른 연산시에도 이용할 거 같으니 모듈화 시켜두겠음.
    root는 단순히 바빌로니아 법을 이용해서 x^(1/2)을 실시하는 함수도 있고 
    뉴튼-랩튼을 이용한 x^(1/y)를 실행하는 함수도 있음.

--- won_ar-35.ino
+++ won_ar-35.ino
@@ -1,1 +1,84 @@
 // 원종완이 쓴 코드는 여기에...
+// 테스트 중... 아직 쓸게 못됨.
+#include <stdio.h>
+
+double abs(double x) {
+	if (x > 0) {
+		return x;
+	}
+	else {
+		return -x;
+	}
+}
+
+double powInte(double x, double y) {
+	double n = x;
+	if (y == 0) {
+		n = 0;
+	}
+	else if (y > 0) {
+		for (int i = 1; i < y; i++) {
+			n *= x;
+		}
+	}
+	else {
+		for (int i = 1; i > y; i--) {
+			n = n / x;
+		}
+	}
+	return n;
+}
+
+double root(double x, int accuracy) {
+	double n = x/2;
+	while (accuracy != 0) {
+		double memory = n;
+		if (powInte(n, 2) == x) {
+			break;
+		}
+		else {
+			n = (powInte(n, 2) + x) / (2 * n);
+			accuracy--;
+			printf("root running now n= %Lf, memory= %Lf, accuracy= %d, bool= %d\n"
+             , n, memory, accuracy, abs(memory - n) < 0.00001);
+			if(abs(memory - n)<0.00001) {
+				break;
+			}
+		}
+	}
+	return n;
+}
+
+double rootInte(double a, double b, int accuracy) {
+	double n = a;
+	while (accuracy != 0) {
+		double memory = n;
+		n = n - ((powInte(n, b) - a) / (b * powInte(n, b - 1)));
+		printf("test rootInte= %f, dy/dx= %f, accuracy= %d\n", n, (b * powInte(n, b - 1)), accuracy);
+		if (abs(memory - n) < 0.00001) {
+			break;
+		}
+		accuracy--;
+	}
+	return n;
+}
+
+void main(){
+	printf("%f\n", powInte(2, 3));
+	printf("%f\n", powInte(2, 0));
+	printf("%f\n", powInte(2, -3));
+	printf("%f\n", root(6, 100));
+	printf("%f\n", rootInte(6, 50, 100));
+	
+	long long int i, a, *ap;
+
+	double fd = -0.01;
+
+	ap = (long long int*)&fd;
+
+	a = *ap;
+
+	for (i = 63; i >= 0; i--){
+		printf("%d", a >> i & 1);
+	}
+}
