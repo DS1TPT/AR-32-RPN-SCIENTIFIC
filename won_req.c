4bfbfba375d8ad69e656d3d9c0fd9515f1ff1b98
won <jonwon2009@naver.com>
2022-05-21 19:29:01 +0900

    mod와 exp를 위한 inteCut 추가, exp 수정

--- won_req.c
+++ won_req.c
@@ -1,18 +1,33 @@
 #include <fp64lib.h> // 64비트 부동소수점
 
+//c 버전
+double calc_inteCut(double x) {
+	double x1 = round(x);
+	if (x1 <= x) {
+		return x -x1;
+	}
+	else {
+		return x - x1 +1.0;
+	}
+}
+//x - round(x)를 대체
+//mod와 exp에 쓰임
+float64_t calc_inteCut(float64_t x){
+	float64_t x1 = fp64_round(x);
+	float64_t result = fp64_sub(x, x1);
+	if(fp64_compare(x1, x) <= 0){
+		return result;
+	}
+	else{
+		return fp64_add(result, fp64_sd(1.0));
+	}
+}
 //실수 범위 모듈러 연산
 //sin연산에 쓰임
 float64_t calc_mod(float64_t x, float64_t y) {
 	float64_t x0 = fp64_div(x, y);
-	float64_t x1 = fp64_round(x0);
-	float64_t x2;
-	if (fp64_compare(x1, x0) <= 0) {
-		x2= fp64_sub(x0, x1);
-	}
-	else {
-		x2 = fp64_add(fp64_sub(x0, x1), fp64_sd(1.0));
-	}
-	return fp64_mul(x2, y);
+	float64_t x1 = calc_inteCut(x0);
+	return fp64_mul(x1, y);
 }
 
 float64_t calc_exp(float64_t x) {
@@ -25,9 +40,9 @@ float64_t calc_exp(float64_t x) {
 	float64_t memory = sum;
 	while (1) {
 		memory = sum;
-		//sum = sum + (u / calc_facto(cnt)) * calc_powInte(x - (int)x, cnt);
+		//sum = sum + (u / calc_facto(cnt)) * calc_powInte(calc_inteCut(x), cnt);
 		sum = fp64_add(sum, fp64_mul(fp64_div(u, calc_facto(fp64_int32_to_float64(cnt))),
-			calc_powInte(fp64_sub(x, fp64_round(x)), fp64_int32_to_float64(cnt))));
+			calc_powInte(calc_inteCut(x), fp64_int32_to_float64(cnt))));
 		if (fp64_compare(calc_abs(fp64_sub(memory, sum)), ACCURACY) == -1) break;
 		cnt++;
 	}
