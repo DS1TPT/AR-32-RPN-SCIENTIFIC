21410c3e6889af7f8f5561117a53681848c90430
won <jonwon2009@naver.com>
2022-05-21 19:09:10 +0900

    Update won_req.c

--- won_req.c
+++ won_req.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <fp64lib.h> // 64비트 부동소수점
 
 //실수 범위 모듈러 연산
@@ -16,3 +15,21 @@ float64_t calc_mod(float64_t x, float64_t y) {
 	return fp64_mul(x2, y);
 }
 
+float64_t calc_exp(float64_t x) {
+	long cnt = 0;
+	float64_t sum = fp64_sd(0.0);
+	float64_t u = calc_powInte(exponentialNum, fp64_round(x));
+	if (fp64_isinf(u) == 1) {
+		return u;
+	}
+	float64_t memory = sum;
+	while (1) {
+		memory = sum;
+		//sum = sum + (u / calc_facto(cnt)) * calc_powInte(x - (int)x, cnt);
+		sum = fp64_add(sum, fp64_mul(fp64_div(u, calc_facto(fp64_int32_to_float64(cnt))),
+			calc_powInte(fp64_sub(x, fp64_round(x)), fp64_int32_to_float64(cnt))));
+		if (fp64_compare(calc_abs(fp64_sub(memory, sum)), ACCURACY) == -1) break;
+		cnt++;
+	}
+	return sum;
+}
